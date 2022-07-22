aba93b107e5d96bacc308ed1ae9b74f49e17011f
MaksimNifedov <ziggy.zen@yandex.ru>
2022-07-22 14:38:06 +0800

    На английском

--- Блок 3/shop.sql
+++ Блок 3/shop.sql
@@ -9,96 +9,96 @@ CREATE SCHEMA IF NOT EXISTS `shop` DEFAULT CHARACTER SET utf8 ;
 USE `shop` ;
 
 -- -----------------------------------------------------
--- Table `shop`.`Раздел`
+-- Table `shop`.`Section`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `shop`.`Раздел` (
-  `ID Раздела` INT NOT NULL AUTO_INCREMENT,
-  `Название` VARCHAR(80) NOT NULL,
-  `Описание` VARCHAR(250) NOT NULL,
-  PRIMARY KEY (`ID Раздела`))
+CREATE TABLE IF NOT EXISTS `shop`.`Section` (
+  `ID_Section` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(80) NOT NULL,
+  `Description` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`ID_Section`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `shop`.`Изображения`
+-- Table `shop`.`Images`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `shop`.`Изображения` (
-  `ID Изображения` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `shop`.`Images` (
+  `ID_Image` INT NOT NULL AUTO_INCREMENT,
   `ALT` VARCHAR(150) NOT NULL,
-  `Адрес изображения` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ID Изображения`),
-  UNIQUE INDEX `Адрес изображения_UNIQUE` (`Адрес изображения` ASC) VISIBLE)
+  `Link_Image` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`ID_Image`),
+  UNIQUE INDEX `Link_Image` (`Link_Image` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `shop`.`Товар`
+-- Table `shop`.`Product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `shop`.`Товар` (
-  `ID товара` INT NOT NULL AUTO_INCREMENT,
-  `Название` VARCHAR(60) NOT NULL,
-  `ID главного раздела` INT NOT NULL,
-  `ID главного  изображения` INT NOT NULL,
-  `Цена` DECIMAL(10,2) NOT NULL,
-  `Цена без скидки` DECIMAL(10,2) NOT NULL,
-  `Цена по промокоду` DECIMAL(10,2) NOT NULL,
-  `Описание` VARCHAR(350) NOT NULL,
-  `Активность товара` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`ID товара`),
-  INDEX `Товар_Раздел` (`ID главного раздела` ASC) VISIBLE,
-  INDEX `Товар_Главное_Изображение` (`ID главного  изображения` ASC) INVISIBLE,
-  CONSTRAINT `Товар_раздел`
-    FOREIGN KEY (`ID главного раздела`)
-    REFERENCES `shop`.`Раздел` (`ID Раздела`)
+CREATE TABLE IF NOT EXISTS `shop`.`Product` (
+  `ID_Product` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(60) NOT NULL,
+  `ID_Main_Section` INT NOT NULL,
+  `ID_Main_Image` INT NOT NULL,
+  `Price` DECIMAL(10,2) NOT NULL,
+  `Price_Without_Sale` DECIMAL(10,2) NOT NULL,
+  `Price_With_Promocode` DECIMAL(10,2) NOT NULL,
+  `Description` VARCHAR(350) NOT NULL,
+  `Product_activity` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`ID_Product`),
+  INDEX `Prodict_Main_Section` (`ID_Main_Section` ASC) VISIBLE,
+  INDEX `Product_Main_Image` (`ID_Main_Image` ASC) INVISIBLE,
+  CONSTRAINT `Product_Main_Section`
+    FOREIGN KEY (`ID_Main_Section`)
+    REFERENCES `shop`.`Section` (`ID_Section`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `Товар_Главное_Изображение`
-    FOREIGN KEY (`ID главного  изображения`)
-    REFERENCES `shop`.`Изображения` (`ID Изображения`)
+  CONSTRAINT `Product_Main_Image`
+    FOREIGN KEY (`ID_Main_Image`)
+    REFERENCES `shop`.`Images` (`ID_Image`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `shop`.`Раздел_Товар`
+-- Table `shop`.`Section_Product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `shop`.`Раздел_Товар` (
-  `Товар_ID товара` INT NOT NULL,
-  `Раздел_ID Раздела` INT NOT NULL,
-  PRIMARY KEY (`Товар_ID товара`, `Раздел_ID Раздела`),
-  INDEX `Раздел_Товар_ID_Товара` (`Товар_ID товара` ASC) VISIBLE,
-  INDEX `Раздел_Товар_ID_раздела` (`Раздел_ID Раздела` ASC) VISIBLE,
-  CONSTRAINT `Раздел_ID_раздела`
-    FOREIGN KEY (`Раздел_ID Раздела`)
-    REFERENCES `shop`.`Раздел` (`ID Раздела`)
+CREATE TABLE IF NOT EXISTS `shop`.`Section_Product` (
+  `Product_ID_Product` INT NOT NULL,
+  `Section_ID_Section` INT NOT NULL,
+  PRIMARY KEY (`Product_ID_Product`, `Section_ID_Section`),
+  INDEX `Section_Product_ID_Product` (`Product_ID_Product` ASC) VISIBLE,
+  INDEX `Section_Product_ID_Section` (`Section_ID_Section` ASC) VISIBLE,
+  CONSTRAINT `Section_ID_Section`
+    FOREIGN KEY (`Section_ID_Section`)
+    REFERENCES `shop`.`Section` (`ID_Section`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `Товар_ID_товара`
-    FOREIGN KEY (`Товар_ID товара`)
-    REFERENCES `shop`.`Товар` (`ID товара`)
+  CONSTRAINT `Product_ID_Product`
+    FOREIGN KEY (`Product_ID_Product`)
+    REFERENCES `shop`.`Product` (`ID_Product`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `shop`.`Товар_изображение`
+-- Table `shop`.`Product_Image`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `shop`.`Товар_изображение` (
-  `ID_товара` INT NOT NULL,
-  `ID_Изображения товара` INT NOT NULL,
-  PRIMARY KEY (`ID_товара`, `ID_Изображения товара`),
-  INDEX `ID_Товара_для_Изображения` (`ID_товара` ASC) VISIBLE,
-  INDEX `Изображение_товара` (`ID_Изображения товара` ASC) VISIBLE,
-  CONSTRAINT `Изображение_ID`
-    FOREIGN KEY (`ID_Изображения товара`)
-    REFERENCES `shop`.`Изображения` (`ID Изображения`)
+CREATE TABLE IF NOT EXISTS `shop`.`Product_Image` (
+  `ID_Prodict` INT NOT NULL,
+  `ID_Image` INT NOT NULL,
+  PRIMARY KEY (`ID_Prodict`, `ID_Image`),
+  INDEX `ID_Product_for_Image` (`ID_Prodict` ASC) VISIBLE,
+  INDEX `Image_Product` (`ID_Image` ASC) VISIBLE,
+  CONSTRAINT `ID_Image`
+    FOREIGN KEY (`ID_Image`)
+    REFERENCES `shop`.`Images` (`ID_Image`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `Товар_ID`
-    FOREIGN KEY (`ID_товара`)
-    REFERENCES `shop`.`Товар` (`ID товара`)
+  CONSTRAINT `ID_Product`
+    FOREIGN KEY (`ID_Prodict`)
+    REFERENCES `shop`.`Product` (`ID_Product`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
